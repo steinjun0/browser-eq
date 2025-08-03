a13e435ba9f60ef66251b1f3287b5306ae9f07c9
steinjun0 <1142308@naver.com>
2025-08-03 19:24:22 +0900

    feat: 테스트용 차트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,88 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import reactLogo from "./assets/react.svg";
 import { useEq } from "./hooks/use-eq";
 import { useGetAudio } from "./hooks/use-get-audio";
+import viteLogo from "/vite.svg";
+
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
+};
 
 function App() {
   const [count, setCount] = useState(0);
 
   const { audio: testSound } = useGetAudio({ path: "/test-sound.mp3" });
 
-  useEq({ audio: testSound });
+  const { getFrequencyResponse } = useEq({ audio: testSound });
+  const [frequneyResponse, setFrequenctresponse] = useState<{
+    frequencyArray: number[];
+    totalMagResponse: number[];
+  }>();
+
+  try {
+    console.log("getFrequencyResponse", getFrequencyResponse());
+  } catch {}
 
   return (
     <>
+      <button
+        onClick={() => {
+          const res = getFrequencyResponse();
+          setFrequenctresponse({
+            frequencyArray: [...res.frequencyArray],
+            totalMagResponse: [...res.totalMagResponse],
+          });
+        }}
+      >
+        getData
+      </button>
+      {frequneyResponse ? (
+        <Line
+          options={options}
+          data={{
+            labels: frequneyResponse.frequencyArray,
+            datasets: [
+              {
+                label: "Dataset 1",
+                data: frequneyResponse.totalMagResponse,
+                borderColor: "rgb(255, 99, 132)",
+                backgroundColor: "rgba(255, 99, 132, 0.5)",
+              },
+            ],
+          }}
+        />
+      ) : null}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
